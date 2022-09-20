2df521dc316b10e9b380149cb27300b0224720af
ETrsw <78511310+ETrsw@users.noreply.github.com>
2022-09-20 14:28:02 -0400

    1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 
 
 const firebaseConfig = {
