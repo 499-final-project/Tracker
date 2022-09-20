f659f2f79ad1d4c1d0415cc1adb445ebc15646d5
ETrsw <78511310+ETrsw@users.noreply.github.com>
2022-09-20 14:19:58 -0400

    commented out  analytics

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ root.render(
   </React.StrictMode>
 );
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
