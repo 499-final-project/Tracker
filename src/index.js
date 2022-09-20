64176e59395745e0ac64967f2e134f5d49929f3e
ETrsw <78511310+ETrsw@users.noreply.github.com>
2022-09-20 14:26:36 -0400

    test

--- src/index.js
+++ src/index.js
@@ -27,8 +27,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
-const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
+ initializeApp(firebaseConfig);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
