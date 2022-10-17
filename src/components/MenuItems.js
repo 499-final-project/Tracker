4eb7dbaa5f719df1a9c7e21009ee2500abf7b6bf
ETrsw <78511310+ETrsw@users.noreply.github.com>
2022-10-17 18:38:09 -0400

    fixed

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -4,7 +4,7 @@ import arrow from '../icons/downArrow.svg';
 import search from '../images/search.png';
 import biden from '../images/biden.png';
 import {Link} from "react-router-dom";
-import TaskList from './TaskList'
+//import TaskList from './TaskList'
 
 
 
@@ -15,11 +15,13 @@ function MenusItems() {
             <li><Link to= "/TaskList">Task List</Link></li>
             <li><Link to= "/UpcomingTask">Upcoming Task</Link></li>
             <li><Link to= "/ContactUs">Contact Us</Link></li>
-            <a className = "more" href = "#" >More</a>
-            <img className = "arrow" src = {arrow}/>
-            <img className = "search" src = {search}/>
-            <img className = "biden" src = {biden}/>
 
+            <a className = "more" href = "/#" >More</a>
+            <img alt = "arrow"className = "arrow" src = {arrow}/>
+            <img alt ="saddg" className = "search" src = {search}/>
+            <img alt = "owl" className = "biden" src = {biden}/>
+           
+            
 
         </div>
     );
