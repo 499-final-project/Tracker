7efe71d1db62017ab98da79a9c11db7d32e77607
Idiled-CS <AndrewPan.CS@gmail.com>
2022-11-25 00:35:38 -0500

    Added Dark mode button

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles/menuitems.css';
-import arrow from '../icons/downArrow.svg';
 
 
 import {Link} from "react-router-dom";
@@ -8,22 +7,33 @@ import { SignInWithGoogle1 } from "../Authentication/signin";
 import { AuthContext } from '../Authentication/AuthContext';
 import { useContext } from "react";
 import { Logout } from "../Authentication/logout";
+import moon from "../icons/moon.png";
+import sun from "../icons/sun.png";
+
 
 
 
 function MenusItems() {
 
  const {currentUser} = useContext(AuthContext);
- 
+
+    const toggle = () =>{
+        const icon = document.getElementById("toggleDark")
+        document.body.classList.toggle("dark")
+        if(document.body.classList.contains("dark")){
+            icon.src = sun;
+        }else{
+            icon.src = moon;
+        }
+    }
     return (  
         <div className = "menu">
           
             <li><Link to= "/TaskList">Task List</Link></li>
             <li><Link to= "/TaskCalendar">Task Calendar</Link></li>     
             <li><Link to= "/ContactUs">Contact Us</Link></li>
-             <li>{ currentUser ? <Logout/> : <SignInWithGoogle1/>}</li> 
-            <a className = "more" href = "/#" >More</a>
-            <img alt = "arrow"className = "arrow" src = {arrow}/>
+            <li>{ currentUser ? <Logout/> : <SignInWithGoogle1/>}</li> 
+            <li><img id="toggleDark" onClick={toggle} src={moon} /></li>
             
            
             
