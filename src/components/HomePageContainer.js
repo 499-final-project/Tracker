989588057e3931d63fdafd0b5d786a0ea14de93f
ETrsw <78511310+ETrsw@users.noreply.github.com>
2022-12-12 15:55:11 -0500

    Added text to homepage and darkmode for create/edit task

--- src/components/HomePageContainer.js
+++ src/components/HomePageContainer.js
@@ -4,9 +4,39 @@ const HomePageContainer = () => {
   return (
     <div>
   
-      <h1>Home Page</h1>
+
       <h4>Welcome to Tracker</h4>
     
+
+      <p>Tracker is a task management app that helps you keep track of your tasks and activities.</p>
+       <p>Click on the "Task List" button to view your task list.</p>
+      <p>Click on the "Create Task" button to add a new task to your list.</p>
+     
+      <p>Here are some ideas for activities to add to your task list:</p>
+      <ul className="homepagelist">
+        <li>Go for a walk</li>
+        <li>Do 10 pushups</li>
+        <li>Drink 8 glasses of water</li>
+        <li>Read a book</li>
+        <li>Do 10 squats</li>
+   
+
+      </ul>
+
+    
+     
+    
+    
+     
+
+
+
+    
+
+
+
+
+
     </div>
     
   );
